d73c748336674aaeb10a485b7a61b078cf3bbc16
iHarryD <anuraagvr555@gmail.com>
2022-06-26 22:50:03 +0530

    feat: add history routes

--- api/historyRoutes.js
+++ api/historyRoutes.js
@@ -1,15 +1,40 @@
 const router = require("express").Router();
 const tokenVerification = require("../middlewares/tokenVerification");
 const users = require("../models/UserModel");
+const videos = require("../models/VideoModel");
 
-router.get("/history/fetch", tokenVerification, (req, res) => {
+router.get("/history", tokenVerification, (req, res) => {
   users.findById(req.user, (err, doc) => {
     if (err) return res.status(500).send({ message: "Something went wrong." });
     res.status(200).send(doc.history);
   });
 });
 
-router.patch("/history/clear", tokenVerification, (req, res) => {
+router.get("/history/fetch-videos", tokenVerification, (req, res) => {
+  users.findById(req.user, (err, doc) => {
+    if (err) return res.status(500).send({ message: "Something went wrong." });
+    videos.find({ _id: { $in: doc.history } }, (err, doc) => {
+      if (err)
+        return res.status(500).send({ message: "Something went wrong." });
+      res.status(200).send(doc);
+    });
+  });
+});
+
+router.post("/history/:id", tokenVerification, (req, res) => {
+  const { id: videoID } = req.params;
+  users.findByIdAndUpdate(
+    req.user,
+    { $addToSet: { history: videoID } },
+    { new: true },
+    (err, doc) => {
+      if (err) return res.status(500).send({ message: err });
+      res.status(200).send(doc.history);
+    }
+  );
+});
+
+router.delete("/history", tokenVerification, (req, res) => {
   users.findByIdAndUpdate(
     req.user,
     { history: [] },
