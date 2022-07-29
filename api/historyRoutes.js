07394966d0da59e99dc826c7e690eb8eefceb0bb
iHarryD <anuraagvr555@gmail.com>
2022-07-29 19:06:08 +0530

    feat: sort history video

--- api/historyRoutes.js
+++ api/historyRoutes.js
@@ -11,12 +11,22 @@ router.get("/history", tokenVerification, (req, res) => {
 });
 
 router.get("/history/fetch-videos", tokenVerification, (req, res) => {
-  users.findById(req.user, (err, doc) => {
+  users.findById(req.user, (err, userDoc) => {
     if (err) return res.status(500).send({ message: "Something went wrong." });
-    videos.find({ _id: { $in: doc.history } }, (err, doc) => {
+    videos.find({ _id: { $in: userDoc.history } }, (err, doc) => {
       if (err)
         return res.status(500).send({ message: "Something went wrong." });
-      res.status(200).send(doc);
+      const sortedHistoryVideos = doc.sort((videoOne, videoTwo) => {
+        if (
+          userDoc.history.indexOf(videoOne._id) >
+          userDoc.history.indexOf(videoTwo._id)
+        ) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+      res.status(200).send(sortedHistoryVideos);
     });
   });
 });
